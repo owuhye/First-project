13af2b49a76147ea7be1774461748c104af9a2d3
jihye kim <owuhye@naver.com>
2025-04-03 08:44:03 +0900

    jihye_munje

---
+++ src/munje_jihye/OperatorEx3.java
@@ -0,0 +1,14 @@
+package munje_jihye;
+
+public class OperatorEx3 {
+
+    public static void main(String[] args) {
+        int score = 80;
+
+//        boolean result = 80 <= score && score <= 100;
+        boolean result = score >= 80 && score <= 100;
+
+        System.out.println(result);
+
+    }
+}
